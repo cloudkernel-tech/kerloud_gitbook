c89fd0e1e9e52a43c5488cbf686a3d7e7ed32739
cloudkernel-tech <coolkesword@163.com>
2020-07-03 20:37:28 +0800

    push gh-pages to main page remote master instead

--- autopub.sh
+++ autopub.sh
@@ -3,7 +3,7 @@
 gitbook build
 
 git checkout master
-git branch -d gh-pages
+git branch -D gh-pages
 
 git checkout -b gh-pages
 
@@ -16,4 +16,6 @@ git clean -fx _book
 
 git add .
 git commit -m 'publish gitbook'
-git push -f origin gh-pages
+
+# push to main page remote
+git push -f mainpage_origin gh-pages:master
